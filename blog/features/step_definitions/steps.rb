52c3ec33b2b0ce60acaf713b1b2424cdd0ced01e
Shiza Shafiq <shizashafiq@hotmail.com>
2016-02-28 14:47:40 +0000

    commit 1

---
+++ blog/features/step_definitions/steps.rb
@@ -0,0 +1,12 @@
+Given(/^I have articles titled (.+)$/)do |titles|
+ titles.split(', ').each do |title|
+end
+end
+
+When(/^I go to the list of articles$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
